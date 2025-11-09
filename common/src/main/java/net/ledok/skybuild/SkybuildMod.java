941239f4f87273c23aa21bbff281a35b04a2f49e
ledok <supermaster.ua.lviv@gmail.com>
2025-11-09 19:23:32 +0100

    Test commit

---
+++ common/src/main/java/net/ledok/skybuild/SkybuildMod.java
@@ -0,0 +1,9 @@
+package net.ledok.skybuild;
+
+public final class SkybuildMod {
+    public static final String MOD_ID = "skybuild";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/net/ledok/skybuild/neoforge/SkybuildModNeoForge.java
@@ -0,0 +1,12 @@
+package net.ledok.skybuild.neoforge;
+
+import net.ledok.skybuild.SkybuildMod;
+import net.neoforged.fml.common.Mod;
+
+@Mod(SkybuildMod.MOD_ID)
+public final class SkybuildModNeoForge {
+    public SkybuildModNeoForge() {
+        // Run our common setup.
+        SkybuildMod.init();
+    }
+}
